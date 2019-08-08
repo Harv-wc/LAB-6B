2819944b46da643e9f4e5b23ca1ed7970e593206
William <harv.wc@gmail.com>
2019-08-08 11:45:40 -0400

    Add project files.

---
+++ NonMotorizedVehicles/Structures.cs
@@ -0,0 +1,42 @@
+﻿using Enumerations;
+using NonMotorizedVehicles;
+
+namespace Structures
+{
+    public struct Tricycle : INonMotorizedVehicle
+    {
+        public TerrainType TerrainType { get; set; }
+        public int NoOfTires { get; set; }
+        public string Brakes { get; set; }
+        public bool HasPedals { get; set; }
+        public int NoOfWheels { get; set; }
+        public bool Moving()
+        {
+            return Moving();
+        }
+        public bool Moving(bool isMoving)
+        {
+            return Moving(isMoving);
+
+        }
+    }
+
+    //added bicycle struct
+    public struct Bicycle : INonMotorizedVehicle
+    {
+        public TerrainType TerrainType { get; set; }
+        public int NoOfTires { get; set; }
+        public string Brakes { get; set; }
+        public bool HasPedals { get; set; }
+        public int NoOfWheels { get; set; }
+        public bool Moving()
+        {
+            return Moving();
+        }
+        public bool Moving(bool isMoving)
+        {
+            return Moving(isMoving);
+        }
+
+    }
+}
